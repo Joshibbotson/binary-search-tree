8e2386837cb10b7fe793120b9b4f52b32cb6e695
Joshua Ibbotson <joshua_ibbotson@hotmail.com>
2022-11-22 21:10:18 +0000

    got delete with children working

--- script.js
+++ script.js
@@ -55,7 +55,14 @@ class Tree {
                     ) {
                         return (node.right = null)
                     } else {
-                        //deleteWithChildren(data, node)//
+                        if (node.right !== null && node.left !== null) {
+                            return (node.right = this.deleteWithChildren(
+                                data,
+                                node.right,
+                                "right"
+                            ))
+                        }
+                        throw Error("single arm child")
                     }
                 }
                 this.delete(data, node.right)
@@ -68,7 +75,14 @@ class Tree {
                     ) {
                         return (node.left = null)
                     } else {
-                        //deleteWithChildren(data, node)//
+                        if (node.right !== null && node.left !== null) {
+                            return (node.left = this.deleteWithChildren(
+                                data,
+                                node.left,
+                                "left"
+                            ))
+                        }
+                        throw Error("single arm child")
                     }
                 }
                 this.delete(data, node.left)
@@ -76,7 +90,78 @@ class Tree {
         }
     }
 
-    deleteWithChildren = (arr, node = this.root) => {}
+    deleteWithChildren = (data, node = this.root, arm) => {
+        console.log("deletechild")
+        let tempNode = node
+        let recFindSmallest = node => {
+            if (node.left === null) {
+                return node
+            }
+            node = node.left
+            return recFindSmallest(node)
+        }
+        let smallest = recFindSmallest(node.right)
+        tempNode.data = smallest.data
+        console.log(tempNode)
+
+        let recRemoveDup = (data, node) => {
+            if (data === node.data) {
+                return (node = node.right)
+            }
+            node = node.left
+            return recRemoveDup(data, node)
+        }
+        let replacementNode = recRemoveDup(tempNode.data, tempNode.right)
+        console.log(replacementNode)
+
+        tempNode.right = replacementNode
+
+        console.log(tempNode)
+        return tempNode
+    }
+
+    //copied from previous code, works to remove single child somewhat//
+    // deleteWithSingleChild = () => {
+    //     console.log("deletechild")
+    //     let tempNode = node
+    //     let recFindSmallest = node => {
+    //         if (node.left === null) {
+    //             return node
+    //         }
+    //         node = node.left
+    //         return recFindSmallest(node)
+    //     }
+    //     let smallest
+    //     if (arm === "right") {
+    //         smallest = recFindSmallest(node.right)
+    //     } else if (arm === "left") {
+    //         smallest = recFindSmallest(node.left)
+    //     }
+    //     tempNode.data = smallest.data
+    //     console.log(tempNode)
+
+    //     let recRemoveDup = (data, node) => {
+    //         if (data === node.data) {
+    //             return (node = node.right)
+    //         }
+    //         node = node.left
+    //         return recRemoveDup(data, node)
+    //     }
+    //     let replacementNode
+    //     if (arm === "right") {
+    //         replacementNode = recRemoveDup(tempNode.data, tempNode.right)
+    //         tempNode.right = replacementNode
+    //     } else if (arm === "left") {
+    //         replacementNode = recRemoveDup(tempNode.data, tempNode.left)
+    //         tempNode.left = replacementNode
+    //     }
+    //     console.log(replacementNode)
+
+    //     // tempNode.right = replacementNode
+
+    //     console.log(tempNode)
+    //     return tempNode
+    // }
 }
 
 const tree = new Tree()
@@ -97,5 +182,5 @@ const prettyPrint = (node, prefix = "", isLeft = true) => {
 }
 
 prettyPrint(tree.root)
-tree.delete(8)
+tree.delete(5)
 prettyPrint(tree.root)
